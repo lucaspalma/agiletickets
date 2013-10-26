9ab78d8c53c83ae7058ce86d7915ebe45d36b647
elton <elton@gmail.com>
2013-10-26 11:22:48 -0200

    movendo inicializacao da Sessao para metodo setup

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,14 +1,21 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
+	
+	Sessao sessao;
+	
+	@Before
+	public void setup(){
+		sessao = new Sessao();
+	}
 
 
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
@@ -16,7 +23,6 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVender2ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(2));
@@ -24,7 +30,6 @@ public class SessaoTest {
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -32,7 +37,6 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
